c34bee57d632bb85a2e779a96d0cdbd1bac7fbb1
jeffgrunewald <jeff.grunewald@gmail.com>
2022-06-06 01:05:06 -0400

    fixing txn sorting function

--- src/helium_follower_service.erl
+++ src/helium_follower_service.erl
@@ -34,7 +34,7 @@ txn_stream(#follower_txn_stream_req_v1_pb{} = Msg, StreamState) ->
       streaming_initialized := StreamInitialized} = grpcbox_stream:stream_handler_state(StreamState),
     txn_stream(Chain, StreamInitialized, SigFun, Msg, StreamState);
 txn_stream(_Msg, StreamState) ->
-    lager:warning("unhandled msg ~p", [_Msg]),
+    lager:warning("unhandled grpc msg ~p", [_Msg]),
     {ok, StreamState}.
 
 -spec init(atom(), grpcbox_stream:t()) -> grpcbox_stream:t().
@@ -54,10 +54,11 @@ handle_info({blockchain_event, {add_block, BlockHash, _Sync, _Ledger}}, StreamSt
     #{chain := Chain, sig_fun := SigFun} = grpcbox_stream:stream_handler_state(StreamState),
     case blockchain:get_block(BlockHash, Chain) of
         {ok, Block} ->
+            {HeadHeight, BlockTime} = head_info(Chain),
             Height = blockchain_block:height(Block),
             EpochStart = blockchain_block_v1:epoch_start(Block),
             SortedTxns = hash_and_sort_txns(Block),
-            _NewStreamState = send_txn_sequence(SortedTxns, Height, EpochStart, SigFun, Chain, StreamState);
+            _NewStreamState = send_txn_sequence(SortedTxns, Height, HeadHeight, BlockTime, EpochStart, SigFun, StreamState);
         _ ->
             lager:error("failed to find block with hash: ~p", [BlockHash]),
             StreamState
@@ -85,7 +86,7 @@ txn_stream(
 ) ->
     lager:debug("subscribing client to txn stream with msg ~p", [_Msg]),
     ok = blockchain_event:add_handler(self()),
-    case process_previous_blocks(Height, Hash, SigFun, Chain, StreamState) of
+    case process_past_blocks(Height, Hash, SigFun, Chain, StreamState) of
         {ok, StreamState1} ->
             HandlerState = grpcbox_stream:stream_handler_state(StreamState1),
             StreamState2 = grpcbox_stream:stream_handler_state(
@@ -97,36 +98,52 @@ txn_stream(
             {grpc_error, {grpcbox_stream:code_to_status(5), <<"requested block not found">>}}
     end.
 
--spec process_previous_blocks(Height      :: pos_integer() | undefined,
-                              TxnHash     :: binary(),
-                              SigFun      :: fun(),
-                              Chain       :: blockchain:blockchain(),
-                              StreamState :: grpcbox_stream:t()) -> {ok, grpcbox_stream:t()} | {error, term()}.
-process_previous_blocks(undefined = _Height, _Hash, _SigFun, _Chain, StreamState) -> {ok, StreamState};
-process_previous_blocks(Height, Hash, SigFun, Chain, StreamState) when is_integer(Height) andalso Height > 0 ->
-    case blockchain:head_block(Chain) of
-        {ok, HeadBlock} ->
-            HeadHeight = blockchain_block:height(HeadBlock),
+-spec process_past_blocks(Height      :: pos_integer() | undefined,
+                          TxnHash     :: binary(),
+                          SigFun      :: fun(),
+                          Chain       :: blockchain:blockchain(),
+                          StreamState :: grpcbox_stream:t()) -> {ok, grpcbox_stream:t()} | {error, term()}.
+process_past_blocks(undefined = _Height, _TxnHash, _SigFun, _Chain, StreamState) ->
+    {ok, StreamState};
+process_past_blocks(Height, TxnHash, SigFun, Chain, StreamState) when is_integer(Height) andalso Height > 0 ->
+    {HeadHeight, BlockTime} = head_info(Chain),
+    case Height > HeadHeight of
+        %% requested a future block; nothing to do but wait
+        true -> {ok, StreamState};
+        false ->
             case blockchain:get_block(Height, Chain) of
-                {ok, PriorBlock} ->
-                    PriorBlockEpoch = blockchain_block_v1:epoch_start(PriorBlock),
-                    PriorBlockTxns = blockchain_block:transactions(PriorBlock),
-                    SortedPriorBlockTxns = hash_and_sort_txns(PriorBlockTxns),
-                    {UnhandledPriorTxns, _} = lists:partition(fun({H1, _T1}) -> H1 > Hash end, SortedPriorBlockTxns),
-                    StreamState1 = send_txn_sequence(UnhandledPriorTxns, Height, PriorBlockEpoch, SigFun, Chain, StreamState),
-                    BlockSeq = lists:seq(Height + 1, HeadHeight),
-                    StreamState2 = lists:foldl(fun(HeightX, StateAcc) ->
-                                                   {ok, BlockX} = blockchain:get_block(HeightX, Chain),
-                                                   EpochX = blockchain_block_v1:epoch_start(BlockX),
-                                                   TxnsX = blockchain_block:transactions(BlockX),
-                                                   SortedTxnsX = hash_and_sort_txns(TxnsX),
-                                                   _NewStateAcc = send_txn_sequence(SortedTxnsX, HeightX, EpochX, SigFun, Chain, StateAcc)
-                                               end, StreamState1, BlockSeq),
-                    {ok, StreamState2};
-                {error, _} = Error -> Error
-            end;
-        {error, _} = Error -> Error
+                {ok, SubscribeBlock} ->
+                    process_past_blocks_(SubscribeBlock, TxnHash, HeadHeight, BlockTime, SigFun, Chain, StreamState);
+                {error, not_found} ->
+                    case blockchain:find_first_block_after(Height, Chain) of
+                        {ok, _Height, ClosestBlock} ->
+                            process_past_blocks_(ClosestBlock, <<>>, HeadHeight, BlockTime, SigFun, Chain, StreamState);
+                        {error, _} = Error -> Error
+                    end
+            end
     end.
+
+-spec process_past_blocks_(StartBlock  :: blockchain_block:block(),
+                           TxnHash      :: binary(),
+                           HeadHeight  :: pos_integer(),
+                           BlockTime   :: non_neg_integer(),
+                           SigFun      :: fun(),
+                           Chain       :: blockchain:blockchain(),
+                           StreamState :: grpcbox_stream:t()) -> {ok, grpcbox_stream:t()}.
+process_past_blocks_(StartBlock, TxnHash, HeadHeight, BlockTime, SigFun, Chain, StreamState) ->
+    StartHeight = blockchain_block:height(StartBlock),
+    BlockEpoch = blockchain_block_v1:epoch_start(StartBlock),
+    SortedStartTxns = hash_and_sort_txns(StartBlock),
+    {UnhandledTxns, _} = lists:partition(fun({H, _T}) -> H > TxnHash end, SortedStartTxns),
+    StreamState1 = send_txn_sequence(UnhandledTxns, StartHeight, HeadHeight, BlockTime, BlockEpoch, SigFun, StreamState),
+    BlockSeq = lists:seq(StartHeight + 1, HeadHeight),
+    StreamState2 = lists:foldl(fun(HeightX, StateAcc) ->
+                                   {ok, BlockX} = blockchain:get_block(HeightX, Chain),
+                                   EpochX = blockchain_block_v1:epoch_start(BlockX),
+                                   SortedTxnsX = hash_and_sort_txns(BlockX),
+                                   _NewStateAcc = send_txn_sequence(SortedTxnsX, HeightX, HeadHeight, BlockTime, EpochX, SigFun, StateAcc)
+                               end, StreamState1, BlockSeq),
+    {ok, StreamState2}.
 
 -spec hash_and_sort_txns(blockchain_block:block()) -> [{binary(), blockchain_txn:txn()}].
 hash_and_sort_txns(Block) ->
@@ -136,36 +153,42 @@ hash_and_sort_txns(Block) ->
 
 -spec send_txn_sequence(SortedTxns :: [{binary(), blockchain_txn:txn()}],
                         Height :: pos_integer(),
+                        HeadHeight :: pos_integer(),
+                        BlockTime :: non_neg_integer(),
                         EpochStart :: pos_integer(),
                         SigFun :: fun(),
-                        Chain :: blockchain:blockchain(),
                         StreamState :: grpcbox_stream:t()) -> grpcbox_stream:t().
-send_txn_sequence(SortedTxns, Height, EpochStart, SigFun, Chain, StreamState) ->
+send_txn_sequence(SortedTxns, Height, HeadHeight, BlockTime, EpochStart, SigFun, StreamState) ->
     lists:foldl(fun(Txn, StateAcc) ->
-                    Msg = encode_follower_resp(Txn, Height, EpochStart, SigFun, Chain),
+                    Msg = encode_follower_resp(Txn, Height, HeadHeight, BlockTime, EpochStart, SigFun),
                     grpcbox_stream:send(false, Msg, StateAcc)
                 end, StreamState, SortedTxns).
 
 -spec encode_follower_resp({TxnHash :: binary(), Txn :: blockchain_txn:txn()},
                            TxnHeight :: pos_integer(),
+                           HeadHeight :: pos_integer(),
+                           BlockTime :: non_neg_integer(),
                            EpochStart :: pos_integer(),
-                           SigFun :: fun(),
-                           Chain :: blockchain:blockchain()) -> follower_pb:follower_resp_v1_pb().
-encode_follower_resp({TxnHash, Txn}, TxnHeight, EpochStart, SigFun, Chain) ->
-    {ok, #block_info_v2{time = BlockTime, height = BlockHeight}} = blockchain:head_block_info(Chain),
+                           SigFun :: fun()) -> follower_pb:follower_resp_v1_pb().
+encode_follower_resp({TxnHash, Txn}, TxnHeight, HeadHeight, BlockTime, EpochStart, SigFun) ->
     BlockAge = erlang:system_time(seconds) - BlockTime,
-    WrappedTxn = #follower_txn_streamed_resp_v1_pb{
+    WrappedEvent = #follower_txn_streamed_resp_v1_pb{
         height = TxnHeight,
         epoch = EpochStart,
         txn_hash = TxnHash,
-        txn = Txn
+        txn = blockchain_txn:wrap_txn(Txn)
     },
     Resp = #follower_resp_v1_pb{
-        height = BlockHeight,
+        height = HeadHeight,
         signature = <<>>,
         block_time = BlockTime,
         block_age = BlockAge,
-        event = {txn_event, WrappedTxn}
+        event = {txn_event, WrappedEvent}
     },
     EncodedRespBin = follower_pb:encode_msg(Resp, follower_resp_v1_pb),
     Resp#follower_resp_v1_pb{signature = SigFun(EncodedRespBin)}.
+
+-spec head_info(Chain :: blockchain:blockchain()) -> {HeadHeight :: pos_integer(), BlockTime :: pos_integer()}.
+head_info(Chain) ->
+    {ok, #block_info_v2{time = BlockTime, height = BlockHeight}} = blockchain:head_block_info(Chain),
+    {BlockHeight, BlockTime}.
